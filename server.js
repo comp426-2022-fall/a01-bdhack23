b4b32a8b9b473dd35b26341a4d58fabb50746de0
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2022-08-29 20:31:34 -0400

    getting minimist ready

--- server.js
+++ server.js
@@ -15,6 +15,26 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 res.end(./public/index.html);
 
 // Require minimist module (make sure you install this one via npm).
+node app.js name=brooke
+
+process.argv.forEach((val, index) => {
+  console.log(`${index}: ${val}`);
+});
+
+const args = process.argv.slice(2);
+
+node app.js brooke
+
+const args = process.argv.slice(2);
+args[0];
+
+const args = require('minimist')(process.argv.slice(2));
+
+args.name; // brooke
+
+npm install minimist
+
+node app.js --name=brooke
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 
