e86e9b7e7173bdcc8513d19ab885a5c5bcf22f97
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2022-09-02 16:33:40 -0400

    Moved fs.readFile to fit order of things

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ const fs = require('fs');
 
 const args = require('minimist')(process.argv.slice(2));
 
+// Require minimist module (make sure you install this one via npm).
+process.argv.forEach((val, index) => {
+  console.log(`${index}: ${val}`);
+});
+
+// Use minimist to process one argument `--port=` on the command line after `node server.js`.
+
+// Define a const `port` using the argument from the command line. 
+// Make this const default to port 3000 if there is no argument given for `--port`.
+const port= args.port || process.env.PORT || 3000
+
+// Use the fs module to create an arrow function using `fs.readFile`.
+// Use the documentation for the Node.js `fs` module. 
+// The function must read a file located at `./public/index.html` and do some stuff with it.
+// The stuff that should be inside this function is all below.
+
 fs.readFile('./public/index.html', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
@@ -13,27 +29,6 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
   }
   console.log(data);
 });
-
-// Require minimist module (make sure you install this one via npm).
-process.argv.forEach((val, index) => {
-  console.log(`${index}: ${val}`);
-});
-
-args.port;
-
-npm install minimist
-
-const port= args.port || process.env.PORT || 3000
-
-// Use minimist to process one argument `--port=` on the command line after `node server.js`.
-
-// Define a const `port` using the argument from the command line. 
-// Make this const default to port 3000 if there is no argument given for `--port`.
-
-// Use the fs module to create an arrow function using `fs.readFile`.
-// Use the documentation for the Node.js `fs` module. 
-// The function must read a file located at `./public/index.html` and do some stuff with it.
-// The stuff that should be inside this function is all below.
 
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
