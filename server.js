b46707c30b28fb2b75259249add58228e6c66c23
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2022-09-03 16:14:15 -0400

    Added more semicolons

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Require http module
-const http = require('http')
+const http = require('http');
 
 // Require fs module
 const fs = require('fs');
@@ -12,7 +12,7 @@ const args = require('minimist')(process.argv.slice(2));
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args.port || 3000
+const port = args.port || 3000;
 
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
