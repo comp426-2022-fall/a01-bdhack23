3319f2bc004ba9ebc7914152c29b2683f535bcfa
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2022-09-07 13:25:07 -0400

    Changing quotation marks again

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
     res.end(data)
   })
   server.listen(port, () => {
-    console.log(``Server listening on port ${port}``)
+    console.log(`Server listening on port ${port}`)
   });
 });
 
